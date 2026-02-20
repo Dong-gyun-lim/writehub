81de4d4b8529694b8e1eadb41b7f1425f7467c13
Gyuni <sfeagle130@naver.com>
2026-02-20 21:23:36 +0900

    refactor: 게시글 공개 범위 기준 변경 (팔로워 -> 구독자)

---
+++ src/main/java/com/writehub/domain/post/entity/Visibility.java
@@ -0,0 +1,6 @@
+package com.writehub.domain.post.entity;
+
+public enum Visibility {
+    PUBLIC,         // 전체 공개
+    SUBSCRIBER_ONLY   // 구독자만
+}
--- src/main/java/com/writehub/writehub/domain/post/entity/Visibility.java
+++
@@ -1,6 +0,0 @@
-package com.writehub.writehub.domain.post.entity;
-
-public enum Visibility {
-    PUBLIC,         // 전체 공개
-    FOLLOWER_ONLY   // 팔로워만
-}
